c072abe031514189f8b85ae23d718178109693c5
eunhy <gp79810@gmail.com>
2026-02-09 14:24:24 +0900

    feat: 8단계 통과
    10+20+30==60
    - 재귀방식을 사용해 다항식 구현

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -2,18 +2,22 @@ package org.example;
 
 public class Calc {
     public static Integer run(String exp){
-        String[] operand=exp.split("[-+*/]");
+        String[] operand=exp.split("[-+*/]",2);
         String[] operator=exp.split("[0-9]");
+
+        if(operand.length==1)
+            return Integer.parseInt(operand[0]);
+
         for (String i :operator){
             switch (i){
                 case "+":
-                    return Integer.parseInt(operand[0]) + Integer.parseInt(operand[1]);
+                    return Integer.parseInt(operand[0]) + run(operand[1]);
                 case "-":
-                    return Integer.parseInt(operand[0]) - Integer.parseInt(operand[1]);
+                    return Integer.parseInt(operand[0]) - run(operand[1]);
                 case "*":
-                    return Integer.parseInt(operand[0]) * Integer.parseInt(operand[1]);
+                    return Integer.parseInt(operand[0]) * run(operand[1]);
                 case "/":
-                    return Integer.parseInt(operand[0]) / Integer.parseInt(operand[1]);
+                    return Integer.parseInt(operand[0]) / run(operand[1]);
 
             }
 
