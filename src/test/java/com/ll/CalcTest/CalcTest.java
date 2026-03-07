518a4ef643d2338a997c519d5b88b5742dea6fd1
eunhy <gp79810@gmail.com>
2026-03-07 21:35:48 +0900

    test: 11단계 테스트
    10 -  10 - 10 - 10 + 10 + 10 - 10 == -10
    - 로직은 9단계의 코드와 동일하나,
    강사님이 문제로 제시하신 문자열은 문자마다 공백이 있었음.

--- src/test/java/com/ll/CalcTest/CalcTest.java
+++ src/test/java/com/ll/CalcTest/CalcTest.java
@@ -69,5 +69,11 @@ public class CalcTest {
         assertThat(Calc.run("10-10-10-10")).isEqualTo(-20);
     }
 
+    @Test
+    @DisplayName("10 -  10 - 10 - 10 + 10 + 10 - 10 == -10")
+    void t11(){
+        assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
+    }
+
 
 }
