e4abb1363dda685985df65f4e56b8b7856dc5b1a
eunhy <gp79810@gmail.com>
2026-03-08 16:24:33 +0900

    feat: 26단계 통과
    -(8 + 2) * -(7 + 3) + 5 == 105
    
    - intoBracket(): 자식 괄호와 그 전 후항만 방문하던 함수에서 자식 괄호까지 모두 방문하는 함수로 고침
    
    -findAdd(): isUnary()함수를 추가하고 이를 통해 연산자가 단항연산자인지 구분함

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -13,65 +13,115 @@ public class Calc {
         //2) findMul 가장뒤에있는 * , /로 수식 파싱해 넘기기 - lastIndexOf()
         //3) findAdd 로 식 넘기기
         Calc calc= new Calc();
-        return calc.intoBracket("("+exp.replace(" ","")+")");
+        return calc.intoBracket(exp.replace(" ",""));
 
     }
+    public int findCloseBracket(String exp){
+        //열린 브라켓을 포함한 문자열을 넣어주면
+        // 그 브라켓의 짝인 닫는 브라켓의 인덱스를 찾아주는 함수
+        int openBracketCount=0;
+        int rightCloseBracketIndex=0;
+        for(char a:exp.toCharArray()){
+            //열린 브라켓
+            if(a=='('){
+                openBracketCount++;
+            }
+            else if(a==')')
+            {
+                openBracketCount--;
+            }
 
+            if(openBracketCount==0){
+                return rightCloseBracketIndex;
+            }
+            rightCloseBracketIndex++;
+        }
+        return -10000;
+    }
 
     public int intoBracket(String exp) {
-        if (exp.contains("(") && exp.contains(")")) {
+        //괄호열기
+        //괄호속 수식 중 가장 가까운 자식 괄호찾기
+        //while로 찾다가 없으면 바로 findAdd를 보내는게 아니라 문자열을 새롭게 만들어서 보내기
+        String expCopy=exp;
+        int startIndex;
+        int endIndex;
 
-            int startIndex = exp.indexOf('(');
-            int endIndex = exp.lastIndexOf(')');
+        int bracketValue;
 
-            int bracketLeftOperand;
-            int bracketRightOperand;
-            int bracketValue;
+        int finalTotalValue=-1000;
 
-            int total=0;
+        StringBuilder newExpSB = new StringBuilder();
 
-            bracketValue=intoBracket(exp.substring(startIndex + 1, endIndex));
-            total+=bracketValue;
+        while(true){
+            if(expCopy.contains("(")){
+                //괄호 찾아서
+                startIndex = expCopy.indexOf('(');
+                endIndex = findCloseBracket(expCopy.substring(startIndex))+startIndex;
 
-            //괄호 이전 항들 있으면 덧셈함수 넣어주기
-            if(startIndex!=0)
-            {
-                if(startIndex<2)
-                    bracketLeftOperand=findAdd("0"+exp.substring(0,startIndex-1));
-                else
-                    bracketLeftOperand=findAdd(exp.substring(0,startIndex-1));
+                //괄호 안의 값 연산
+                bracketValue=intoBracket(expCopy.substring(startIndex+1,endIndex));
 
-                    //마이너스를 붙이고
-                total=evaluateExpr(bracketLeftOperand,total,exp.charAt(startIndex-1));
+                //연산값 및 괄호 전후 수식 전부 새 수식에 붙이기
+                //괄호전
+                newExpSB.append(expCopy, 0, startIndex);
+                //괄호값
+                newExpSB.append(bracketValue);
+
+                // 자식관계의 괄호를 모두 탐색하기 위해 처리 문자열을
+                //이전에 처리한 괄호의 뒷부분으로 서브스트링
+                expCopy=expCopy.substring(endIndex+1);
+
+            } else {
+                //괄호 후 값 붙이고 연산 들어가기
+                newExpSB.append(expCopy);
+                expCopy = "";
+                break;
             }
-            //괄호 이후 항들 있으면 덧셈함수 넣어주기
-            if(endIndex!=exp.length()-1){
-                bracketRightOperand=findAdd(exp.substring(endIndex+2));
-                total=evaluateExpr(total,bracketRightOperand,exp.charAt(endIndex+1));
-            }
 
-            return total;
-        } else if (exp.contains("(") ^ exp.contains(")")){
-            System.exit(-1);
         }
-        else //만약 괄호가 더이상 없다면
-        {
-            return findAdd(exp);
-            //곱셈함수 불러서 리턴
-        }
-        return -1;
+        finalTotalValue=findAdd(newExpSB.toString());
+
+        return finalTotalValue;
+
     }
 
-
+    boolean isUnary(String exp, int index){
+        //선택한 연산자가 단항 연산자인지 True False를 뱉는 함수
+        // 단항연산자일 조건
+        // -맨앞에 있는 연산자
+        // -다른 연산자와 연달아 있는 연산자
+        if(index<1)
+            //맨앞에 있거나 연산자가 존재하지 않을 때도 필터링가능
+            return true;
+        //연산자가 앞에 붙어있으면
+        return exp.charAt(index - 1) < 48;
+    }
     public int findAdd(String exp){
+        //가장 오른쪽에 있는 연산자 찾기
         int operatorIndex=Math.max(exp.lastIndexOf('+'),exp.lastIndexOf('-'));
 
-        //오른쪽이 단항->operatorIndex에서 문제 일으킬거임 : 7-(-)8 -> 왼쪽 인덱스를 확인
-        //왼쪽이 단항->얘도 결국 혼자 남았을때 operatorIndex에서 문제 일으킴 (-)8 -> 인덱스값 자체를 확인
+        if(operatorIndex>0){ //연산자가 있으면
+            if(isUnary(exp,operatorIndex)){//연산자가 단항 연산자인지 검색
+                int rightOperand;
+                int leftOperand;
 
-        if(operatorIndex>0&&exp.charAt(operatorIndex-1)>=48){ //연산자가 있으면
-        //if(operatorIndex>0){ //연산자가 있으면
-            int leftOperand= findAdd(exp.substring(0,operatorIndex));
+                if(operatorIndex==0) {//단항연산자가 맨앞에 있을떄
+                    int result=findMul(exp.substring(operatorIndex));
+                    return result;
+
+                }
+                else {//단항연산자가 중간에 있을때
+                    leftOperand= findAdd(exp.substring(0,operatorIndex-1));
+                    rightOperand = findMul(exp.substring(operatorIndex));
+                    return evaluateExpr(leftOperand,rightOperand,exp.charAt(operatorIndex-1));
+                }
+
+
+            }
+
+
+            int leftOperand=findAdd(exp.substring(0,operatorIndex));
             int rightOperand=findMul(exp.substring(operatorIndex+1));
 
             return evaluateExpr(leftOperand,rightOperand,exp.charAt(operatorIndex));
